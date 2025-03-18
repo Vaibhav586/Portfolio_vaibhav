61ac5aac49e774dc859d9956fbc40f73e3dc4e64
Vaibhav <111916301+Vaibhav586@users.noreply.github.com>
2025-03-19 05:08:14 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import { Hero } from './components/Hero';
 import { About } from './components/About';
 import { Skills } from './components/Skills';
 import { Projects } from './components/Projects';
-import { Certifications } from "./components/Certifications";
-import { Experience } from "./components/Experience";
 import { Contributions } from "./components/Contributions";
 import { Contact } from './components/Contact';
 
@@ -15,8 +13,6 @@ function App() {
       <About />
       <Skills />
       <Projects />
-       <Certifications />
-      <Experience />
       <Contributions />
       <Contact />
     </div>
