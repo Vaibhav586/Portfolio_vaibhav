df1b5f9c8f1c6f577d0f747ce7b18ff39d00ba07
Vaibhav <111916301+Vaibhav586@users.noreply.github.com>
2025-03-19 04:18:01 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      'react-github-calendar': 'react-github-calendar/es/index.js', // try 'cjs' or 'umd' too if needed
-    },
+  optimizeDeps: {
+    exclude: ['lucide-react'],
   },
 });
