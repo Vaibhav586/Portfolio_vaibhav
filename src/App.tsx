d97986d6a08481548edd60b8a06d278e905b8727
Vaibhav <111916301+Vaibhav586@users.noreply.github.com>
2025-03-19 04:29:59 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import { Hero } from './components/Hero';
 import { About } from './components/About';
 import { Skills } from './components/Skills';
 import { Projects } from './components/Projects';
-import { Certifications } from "./components/Certifications";
-import { Experience } from "./components/Experience";
-import { Contributions } from "./components/Contributions";
+import { Certifications } from "./components/Certifications.tsx";
+import { Experience } from "./components/Experience.tsx";
+import { Contributions } from "./components/Contributions.tsx";
 import { Contact } from './components/Contact';
 
 function App() {
